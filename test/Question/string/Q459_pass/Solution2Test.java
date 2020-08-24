36ccb30455fc9a29f568df99cfc60b8893fc8211
navy <sdn_app@163.com>
2020-08-24 17:34:26 +0800

    8-24 commit

---
+++ test/Question/string/Q459_pass/Solution2Test.java
@@ -0,0 +1,16 @@
+package Question.string.Q459_pass;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Solution2Test {
+
+    @Test
+    public void repeatedSubstringPattern() {
+        assertTrue(new Solution2().repeatedSubstringPattern("abab"));
+        assertFalse(new Solution2().repeatedSubstringPattern("aba"));
+        assertTrue(new Solution2().repeatedSubstringPattern("abcabcabcabc"));
+    }
+}
