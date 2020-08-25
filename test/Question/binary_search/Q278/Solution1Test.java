1a4e7f8233c3650973ffb8beda5b2b0f85edc477
navy <sdn_app@163.com>
2020-08-25 17:39:16 +0800

    8-25 commit

---
+++ test/Question/binary_search/Q278/Solution1Test.java
@@ -0,0 +1,13 @@
+package Question.binary_search.Q278;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution1Test {
+
+    @Test
+    public void firstBadVersion() {
+        new Solution1().firstBadVersion(5);
+    }
+}
