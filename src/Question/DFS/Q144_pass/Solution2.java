8716234978e22a40a61cebba91f7822f72522fdc
navy <sdn_app@163.com>
2020-09-28 17:44:08 +0800

    9-27 commit

---
+++ src/Question/DFS/Q144_pass/Solution2.java
@@ -0,0 +1,55 @@
+/**
+ 144. 二叉树的前序遍历
+ 给定一个二叉树，返回它的 前序 遍历。
+
+ 示例:
+
+ 输入: [1,null,2,3]
+ 1
+  \
+   2
+  /
+ 3
+
+ 输出: [1,2,3]
+ 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ */
+
+package Question.DFS.Q144_pass;
+
+import Question.Tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ 执行用时：
+ 0 ms, 在所有 Java 提交中击败了100.00%的用户
+ 内存消耗：
+ 37.9 MB, 在所有 Java 提交中击败了69.03%的用户
+ */
+public class Solution2 {
+    public List<Integer> preorderTraversal(TreeNode root){
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        preorder(root, result);
+
+        System.out.println(result);
+        return result;
+
+    }
+
+    private void preorder(TreeNode node, ArrayList list) {
+        if (node == null) {
+            return;
+        }
+
+        list.add(node.val);
+        preorder(node.left, list);
+        preorder(node.right,list);
+    }
+
+
+}
---
+++ test/Question/DFS/Q144_pass/Solution1Test.java
@@ -0,0 +1,40 @@
+package Question.DFS.Q144_pass;
+
+import Question.Tree.TreeNode;
+import org.junit.Test;
+
+public class Solution1Test {
+
+    /**
+     *              1
+     *           /    \
+     *          2     3
+     *        /   \   /
+     *       4    5  6
+     *
+     *    124536
+     */
+
+    @Test
+    public void preorderTraversal() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(6);
+
+
+        root.left = node1;
+        root.right = node2;
+
+        node1.left = node3;
+        node1.right = node4;
+
+        node2.left = node5;
+
+        new Solution1().preorderTraversal(root);
+    }
+
+
+}
