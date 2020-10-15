7d4060ce4874028491d7cbe8f4d9754190b01f32
navy <sdn_app@163.com>
2020-10-15 17:48:41 +0800

    10-15 commit

---
+++ test/Question/BFS/Q116/Solution1Test.java
@@ -0,0 +1,22 @@
+package Question.BFS.Q116;
+
+import Question.Node;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution1Test {
+
+    @Test
+    public void connect() {
+        Node n7 = new Node(7);
+        Node n6 = new Node(6);
+        Node n5 = new Node(5);
+        Node n4 = new Node(4);
+        Node n3 = new Node(3);
+        Node n2 = new Node(2,n4,n5,null);
+        Node n1 = new Node(1);
+
+
+    }
+}
